9ab2581fcda130f3b5cfa6fd3350f81cdc912961
dipanker <dipanker_kr@outlook.com>
2022-07-29 02:25:02 +0530

    changed server start message added ConsoleColor.java

--- src/main/java/com/test/restapi/RestapiApplication.java
+++ src/main/java/com/test/restapi/RestapiApplication.java
@@ -1,5 +1,6 @@
 package com.test.restapi;
 
+import com.test.restapi.util.ConsoleColor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +9,8 @@ public class RestapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestapiApplication.class, args);
-		System.out.println("hello, the rest api application started.");
+
+		System.out.println("\n==============================================\n||" + ConsoleColor.YELLOW_BRIGHT + "\t\trestapi application started :)\t\t" + ConsoleColor.RESET + "||\n==============================================\n");
 	}
 
 }
