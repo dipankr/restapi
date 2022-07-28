d7169453b4ffd7a11c19cd4910a8420b1ac0bc1a
dipanker <dipanker_kr@outlook.com>
2022-07-29 02:06:30 +0530

    added some methods to consume self APIs as well as external APIs added redirects added notes added json to the maven dependency

--- src/main/java/com/test/restapi/controller/GreetController.java
+++ src/main/java/com/test/restapi/controller/GreetController.java
@@ -1,11 +1,18 @@
 package com.test.restapi.controller;
 
+import com.test.restapi.util.HTMLUtil;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import java.io.StringReader;
+import java.net.URISyntaxException;
 
 @RestController
 public class GreetController {
@@ -13,7 +20,7 @@ public class GreetController {
     @GetMapping("/")
     public ResponseEntity<?> success(){
         String msg = "Server up and running: RestAPI";
-        return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT).body(msg);
+        return ResponseEntity.status(HttpStatus.OK).body(msg);
     }
 
     @GetMapping("/greet")
@@ -22,6 +29,51 @@ public class GreetController {
     }
     @PostMapping
     public ResponseEntity<?> sayHelloWithName(@RequestBody String name){
-        return ResponseEntity.ok("Hello "+name+"! Welcome to the rest api application using spring boot");
+        return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT).body("Hello "+name+"! Welcome to the rest api application using spring boot");
+    }
+
+    @GetMapping("/self")
+    public String apiSelf(){
+        String uri = "http://localhost:80/";
+        RestTemplate rt = new RestTemplate();
+        String result = rt.getForObject(uri, String.class);
+        return result;
+    }
+
+    @GetMapping("/meow")
+    @ResponseBody
+    public String apiCat(){
+        String uri = "https://github.com/wh-iterabb-it/meowfacts";
+        RestTemplate rt = new RestTemplate();
+        String result = rt.getForObject(uri, String.class);
+        return result;
+    }
+
+    @RequestMapping(value = "/dogs", produces = MediaType.TEXT_HTML_VALUE)
+    public String apiRandDog() {
+        String uri = "https://random.dog/woof.json";
+        RestTemplate rt = new RestTemplate();
+        String res = rt.getForObject(uri, String.class);
+
+        //fetch only images/gifs
+        while(!(null == res || res.endsWith(".jpg\"}") || res.endsWith(".jpeg\"}") || res.endsWith(".png\"}") || res.endsWith(".gif\"}"))){
+            res = rt.getForObject(uri, String.class);
+        }
+
+        //parse the url
+        JsonReader reader = Json.createReader(new StringReader(res));
+        JsonObject json = reader.readObject();
+        String url = json.getString("url").substring(8).toLowerCase();
+        System.out.println("url = https://" + url);
+
+        String t = HTMLUtil.getDogViewHTML(url);
+        return t;
+    }
+
+    @GetMapping("/redir")
+    public RedirectView redir() throws URISyntaxException {
+        RedirectView redirectView = new RedirectView();
+        redirectView.setUrl("http://www.google.com");
+        return redirectView;
     }
 }
---
+++ src/main/java/com/test/restapi/util/HTMLUtil.java
@@ -0,0 +1,16 @@
+package com.test.restapi.util;
+
+public class HTMLUtil {
+    public static String getDogViewHTML(String url){
+        return "<html style=\"height: 100%;\">\n" +
+                "    <head>\n" +
+                "        <meta name=\"viewport\" content=\"width=device-width, minimum-scale=0.1\">\n" +
+                "        <title>Dog</title>\n" +
+                "    </head>\n" +
+                "    <body style=\"margin: 0px; background: #0e0e0e; height: 100%\">\n" +
+                "        <img style=\"display: block;-webkit-user-select: none;margin: auto;cursor: zoom-in;background-color: hsl(0, 0%, 90%);transition: background-color 300ms;\"\n" +
+                "        src=\"https://"+url+"\" width=auto height=100%>\n" +
+                "    </body>\n" +
+                "</html>";
+    }
+}
