73397fc3f76842ca4f926690e9fc0b2d6bd7dd64
dipanker <dipanker_kr@outlook.com>
2022-07-28 17:06:07 +0530

    modified the annotation to specific request type annotations added new methods to interact with request paths, and post request using response entity for returning responses with proper status codes using the request body of the post request to embed in the response modified the server port

--- src/main/java/com/test/restapi/controller/GreetController.java
+++ src/main/java/com/test/restapi/controller/GreetController.java
@@ -1,13 +1,27 @@
 package com.test.restapi.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetController {
 
-    @RequestMapping("/greet")
-    public String sayHello(){
-        return "Hello! this is a rest api application.";
+    @GetMapping("/")
+    public ResponseEntity<?> success(){
+        String msg = "Server up and running: RestAPI";
+        return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT).body(msg);
+    }
+
+    @GetMapping("/greet")
+    public ResponseEntity<?> sayHello(){
+        return ResponseEntity.badRequest().body("Hello! Greetings from the rest api application.");
+    }
+    @PostMapping
+    public ResponseEntity<?> sayHelloWithName(@RequestBody String name){
+        return ResponseEntity.ok("Hello "+name+"! Welcome to the rest api application using spring boot");
     }
 }
